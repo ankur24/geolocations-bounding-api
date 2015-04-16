14d8467775adfe448d83e486358317e3cde98bbc
ankur24 <ankur.nayak24@gmail.com>
2015-04-16 14:17:21 +0530

    Update mongofindbounding.py

--- mongofindbounding.py
+++ mongofindbounding.py
@@ -1,14 +1,38 @@
-import csv
-#from pymongo import MongoClient
 
-mongohost = "d7"
-mongoport = 27017
-#client = MongoClient(mongohost, mongoport)
-#db = client.entities
-cities = db.cities
-docs = [doc for doc in cities.find({"loc": {"$within": {"$box": [[2, 2], [5, 6]]}}})]
+#docs = [doc for doc in cities.find({"loc": {"$within": {"$box": [[2, 2], [5, 6]]}}})]
+
+#docs = [doc for doc in cities.find({"loc": {"$geoWithin": {"$box": [[2, 2], [5, 6]]}}})]
 
 #give list geolocation points output
 #list of clustered list of geopoints
-def cluster(docs):
-    pass
+from sklearn import cluster
+from pymongo import MongoClient, GEO2D
+import numpy as np
+import collections
+
+#mongohost = "ds049170.mongolab.com:49170/commondb"
+#mongoport = 49170
+client = MongoClient("mongodb://user:pass@ds049170.mongolab.com:49170/commondb")
+db = client.commondb
+cities = db.cities
+
+docs = [doc.loc for doc in cities.find({"loc": {"$geoWithin": {"$box": [[1.5, 42], [2, 45]]}}})]
+#print docs
+if len(docs) > 10:
+    #docsa =np.array(docs)
+    average_linkage = cluster.AgglomerativeClustering(
+        linkage="average", affinity="cityblock", n_clusters=10,
+        connectivity=connectivity)
+    average_linkage.fit(docs)
+    y_pred = average_linkage.labels_.astype(np.int)
+    print pool(docs, y_pred)
+else:
+    print zip(*docs, 0*len(docs))
+
+def pool(docsa, y_pred):
+    tens = collections.defaultdict(labmda: [0,0,0])
+    for i in len(docs):
+        tens[y_pred[i]][0] += docs[i][0]
+        tens[y_pred[i]][1] += docs[i][1]
+        tens[y_pred[i]][1] += 1
+    return tens
