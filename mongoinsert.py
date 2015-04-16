c02313c28b689db5e7d6c51d42b45f6ab1bdc1d2
ankur24 <ankur.nayak24@gmail.com>
2015-04-16 14:16:27 +0530

    Update mongoinsert.py

--- mongoinsert.py
+++ mongoinsert.py
@@ -1,18 +1,26 @@
 import csv
-from pymongo import MongoClient
+from pymongo import MongoClient, GEO2D
 
-mongohost = "d7"
-mongoport = 27017
-client = MongoClient(mongohost, mongoport)
-db = client.entities
-cities = db.cities 
-cities.create_index([("loc", GEO2D)])
+#mongohost = "ds049170.mongolab.com:49170/commondb"
+#mongoport = 49170
+client = MongoClient("mongodb://user:pass@ds049170.mongolab.com:49170/commondb")
+db = client.commondb
+cities = db.cities
+temp = []
 with open('wcp.csv') as csvfile:
     reader = csv.DictReader(csvfile)
-    for row in reader:
-        print float(row['Longitude']),float(row['Latitude'])
+    for i,row in enumerate(reader):
+        #if i <= 1000: continue
         row['loc'] = [float(row['Longitude']),float(row['Latitude'])]
         del row['Longitude']
         del row['Latitude']
-        #db.cities.insert_one(row)
-        break
+        temp.append(row)
+        if i % 50 == 0:
+            db.cities.insert(temp)
+            temp = []
+        #print float(row['Longitude']),float(row['Latitude'])
+        #break
+        #if i > 1000: break
+    if temp:
+        db.cities.insert(temp)
+cities.create_index([("loc", GEO2D)])
